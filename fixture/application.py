47d81afe0b2dcdaeb8d4551b896586be4b4959bd
Novikova.Olga <aquarius519@gmail.com>
2019-10-15 18:15:01 +0300

    тест без ркодера + одна сессия (не работает)

--- fixture/application.py
+++ fixture/application.py
@@ -4,6 +4,7 @@ from fixture.group import GroupHelper
 
 
 class Application:
+
     def __init__(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
--- fixture/group.py
+++ fixture/group.py
@@ -3,9 +3,9 @@ class GroupHelper:
     def __init__(self,app):
         self.app = app
 
-    def return_to_groups_page(self):
+    def open_groups_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("group page").click()
+        wd.find_element_by_link_text("groups").click()
 
     def create(self, group):
         wd = self.app.wd
@@ -26,7 +26,16 @@ class GroupHelper:
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
 
-    def open_groups_page(self):
+    def delete_first_group(self):
         wd = self.app.wd
-        # open groups page
-        wd.find_element_by_link_text("groups").click()
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+
+    def return_to_groups_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("group page").click()
+
